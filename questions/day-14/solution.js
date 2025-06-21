a1dafaafdeaee60a412689fdc75437cf41206c0b
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-06-21 23:28:50 +0530

    added solution file

---
+++ questions/day-14/solution.js
@@ -0,0 +1,24 @@
+// 🚀 Day 14 – Throttle Function Implementation
+
+const throttle = (fn,delay)=>{
+    const last = 0
+
+    return function (...args) {
+        const now = Date.now()
+        if (now - last === delay){
+            last = Date.now()
+             fn.apply(this, args);
+        }
+    }
+}
+
+const throttledLog = throttle(() => {
+  console.log('Called at', Date.now());
+}, 2000);
+
+// Simulate rapid calls
+throttledLog();
+throttledLog();
+throttledLog();
+
+setTimeout(throttledLog, 2200); // This one should fire
